387839407c094ca50b3ea7bd9fa8858aaec73104
BrandonKR1 <153046372+BrandonKR1@users.noreply.github.com>
2026-04-06 18:29:58 +1000

    Add files via upload

---
+++ Batch apply object classifiers.groovy
@@ -0,0 +1,6 @@
+//Batch apply object classifiers. This script should be used to batch apply pre-trained object classifiers to all (or a subset) of images in the project. 
+// Just replace the classifier names below with your pretrained classifiers. Press the three dots on the bottom right of the window and click 'Run for project'.
+// Then choose which images to apply the script to. Then you're done :)
+
+selectAnnotations();
+runObjectClassifier("classifier1", "classifier2", "classifier3");
